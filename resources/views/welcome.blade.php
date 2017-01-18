f4831cff8225e19754464b2075647e0089f6fc6c
Phil Stephens <emailtheprivateer@gmail.com>
2017-01-18 13:56:15 +1000

    remove top links from welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,17 +73,6 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
                     {{ config('app.name', 'CloudStage') }}
